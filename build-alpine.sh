09b3606451cd5834fc6e920e4a7bd4aed08db12c
zhaixiaojuan <zhaixiaojuan@loongson.cn>
2024-03-12 20:13:27 +0800

    Modify alpine to support loongarch64

---
+++ build-alpine.sh
@@ -0,0 +1,3 @@
+./configure --build=loongarch64-alpine-linux-musl  --host=loongarch64-alpine-linux-musl --prefix=/usr --sysconfdir=/etc --mandir=/usr/share/man --localstatedir=/var
+make
+make install
